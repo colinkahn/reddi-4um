d92d5e565a599899cdd837579557b80d3f9c1636
Colin Kahn <colin@magneticcreative.com>
2012-11-13 23:56:15 -0800

    Move import up

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+import redis
+import os
+
 # key must be 16, 24 or 32 bytes long
 SECRET_KEY = 'SECRET_KEY'
 
@@ -10,9 +13,6 @@ DEBUG = True
 ALLOWED_TAGS = ['a', 'b', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'br', 'h1', 'h2', 'u']
 
 TOPICS_PER_PAGE = 1
-
-import redis
-import os
 
 REDIS_URL = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 DATABASE = redis.from_url(REDIS_URL)
